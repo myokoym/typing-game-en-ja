3b73b0da03ceb54b82feff1c7927af4d0deea5ca
Masafumi Yokoyama <yokoyama@clear-code.com>
2019-01-06 23:15:09 +0900

    Extract battle.rb

---
+++ battle.rb
@@ -0,0 +1,51 @@
+module MyTyping
+  class Battle
+    def initialize
+      @font = Gosu::Font.new(20)
+      @score = 0
+      @input_text = ""
+      @current_key = nil
+      @comment = nil
+      @comment_time = 0
+      @dic = CSV.read("EJDict/release/ejdic-hand-utf8.txt", col_sep: "\t", quote_char: "_").to_a
+      @current_word = @dic.sample
+    end
+
+    def update
+      if @current_word[0].downcase == @input_text
+        @comment = "complete!"
+      end
+      if @comment
+        @comment_time += 1
+      end
+      if @comment_time > 100
+        @score += 1
+        @comment = nil
+        @comment_time = 0
+        @current_word = @dic.sample
+        @input_text = ""
+      end
+    end
+
+    def draw
+      @font.draw(@current_word[0], 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+      @font.draw(@input_text, 10, 30, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+      @font.draw(@current_word[1], 10, 50, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW) if @comment
+      @font.draw(@comment, 10, 70, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+      @font.draw(@score, 10, 90, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+      @font.draw(@comment_time, 10, 110, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW) if DEBUG
+    end
+
+    def button_down(id)
+      case id
+      when Gosu::KB_BACKSPACE
+        @input_text.chop!
+      else
+        char = Gosu.button_id_to_char(id)
+        if char != @current_key
+          @input_text << char
+        end
+      end
+    end
+  end
+end
--- window.rb
+++ window.rb
@@ -1,5 +1,6 @@
 require "gosu"
 require "csv"
+require "./battle"
 
 module MyTyping
   DEBUG = false
@@ -12,52 +13,23 @@ module MyTyping
     def initialize
       super 640, 480
       self.caption = "MyTyping"
-      @font = Gosu::Font.new(20)
-      @score = 0
-      @input_text = ""
-      @current_key = nil
-      @comment = nil
-      @comment_time = 0
-      @dic = CSV.read("EJDict/release/ejdic-hand-utf8.txt", col_sep: "\t", quote_char: "_").to_a
-      @current_word = @dic.sample
+      @battle = Battle.new
     end
 
     def update
-      if @current_word[0].downcase == @input_text
-        @comment = "complete!"
-      end
-      if @comment
-        @comment_time += 1
-      end
-      if @comment_time > 100
-        @score += 1
-        @comment = nil
-        @comment_time = 0
-        @current_word = @dic.sample
-        @input_text = ""
-      end
+      @battle.update
     end
 
     def draw
-      @font.draw(@current_word[0], 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-      @font.draw(@input_text, 10, 30, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-      @font.draw(@current_word[1], 10, 50, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW) if @comment
-      @font.draw(@comment, 10, 70, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-      @font.draw(@score, 10, 90, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-      @font.draw(@comment_time, 10, 110, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW) if DEBUG
+      @battle.draw
     end
 
     def button_down(id)
       case id
       when Gosu::KB_ESCAPE
         close
-      when Gosu::KB_BACKSPACE
-        @input_text.chop!
       else
-        char = Gosu.button_id_to_char(id)
-        if char != @current_key
-          @input_text << char
-        end
+        @battle.button_down(id)
         super
       end
     end
